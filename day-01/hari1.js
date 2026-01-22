cec1ed4af16096aaaed84578690be376d30c85f6
Marwan <marwanshah@gmail.com>
2026-01-23 01:24:15 +0800

    Day 1: JS Basic variables and output

---
+++ day-01/hari1.js
@@ -0,0 +1,12 @@
+//Maklumat barang
+let namaBarang = "Keyboard";
+let harga = 120;
+let stok = 5;
+
+console.log ("Barang:", namaBarang);
+console.log ("Harga: RM" + harga);
+console.log ("Stock", stok, "unit");
+
+//tambah stok
+stok = stok + 3;
+console.log("Stock Baru:", stok)
